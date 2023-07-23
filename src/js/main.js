52871f90e04496572e9df965e3ef8c4af7f5c3e2
KEO <keoxcoelho@gmail.com>
2023-07-23 18:06:44 -0300

    cursos pointer btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 // Ola, boas-vindas ao meu projeto de Lembretes!
 // Conforme combinado com a recrutadora Nara, meu projeto foi feito no JavaScript
 
-// add event listener to wait for DOM to be loaded
 document.addEventListener('DOMContentLoaded', () => {
   const reminderForm = document.getElementById('reminder-form');
   const errorMessage = document.getElementById('error-message');
@@ -17,34 +16,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const reminderName = document.getElementById('reminder-name').value;
     const reminderDate = document.getElementById('reminder-date').value;
 
-    // validate if "Lembrete" field is filled in
+    // validate if the "Lembrete" field is filled in
     if (!reminderName) {
       errorMessage.textContent = 'Preencha o campo "Lembrete" para continuar.';
       return;
     }
 
-    // validate that "Data" field is filled and is a valid date in future
-    const currentDate = new Date(); // get current date
+    // validate that the "Data" field is filled and is a valid date in the future
+    const currentDate = new Date(); // Fix: Get the current date
     const selectedDate = new Date(reminderDate);
+
+    // Add one day to the selected date
+    selectedDate.setDate(selectedDate.getDate() + 1);
+
     if (!reminderDate || selectedDate <= currentDate) {
       errorMessage.textContent = 'Poxa, não deu certo! A data deve ser no futuro.';
       return;
     }
 
-    // if fields are valid, clear error message and add reminder
+    // if the fields are valid, clear the error message and add the reminder
     errorMessage.textContent = '';
-    addReminderToList(reminderName, reminderDate);
+    addReminderToList(reminderName, selectedDate);
     reminderForm.reset();
 
     // saves reminder in local storage
-    const reminderData = { name: reminderName, date: reminderDate };
+    const reminderData = { name: reminderName, date: selectedDate.toISOString() };
     savedReminders.push(reminderData);
     savedReminders.sort((a, b) => new Date(a.date) - new Date(b.date)); // Sort reminders by date
     localStorage.setItem(localStorageKey, JSON.stringify(savedReminders));
-
-    alert('Lembrete criado!');
   });
-
 
   function addReminderToList(name, date) {
     const reminderItem = document.createElement('div');
@@ -58,8 +58,8 @@ document.addEventListener('DOMContentLoaded', () => {
       <span class="delete-btn" onclick="deleteReminder(this)" data-name="${name}">x</span>
     `;
 
-    // find correct position to insert new reminder
-    const currentDate = new Date(); // get current date
+    // Find the correct position to insert the new reminder
+    const currentDate = new Date(); // Fix: Get the current date
     const selectedDate = new Date(date);
     const reminderItems = remindersContainer.getElementsByClassName('reminder-item');
     let insertIndex = 0;
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
       insertIndex++;
     }
 
-    // insert new reminder at correct position
+    // Insert the new reminder at the correct position
     if (insertIndex === reminderItems.length) {
       remindersContainer.appendChild(reminderItem);
     } else {
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // function to format date in pattern dd/mm/aaaa
+  // function to format date in the pattern dd/mm/aaaa
   function formatDate(dateString) {
     const date = new Date(dateString);
     const day = date.getDate().toString().padStart(2, '0');
@@ -96,13 +96,9 @@ function deleteReminder(deleteButton) {
   // remove reminder from local storage
   const localStorageKey = 'lembretes';
   const savedReminders = JSON.parse(localStorage.getItem(localStorageKey)) || [];
-  const reminderName = deleteButton.dataset.name; // get name of reminder by attribute 'data-name'
+  const reminderName = deleteButton.dataset.name; // get the name of reminder by attribute 'data-name'
   const updatedReminders = savedReminders.filter(
     (reminder) => reminder.name !== reminderName
   );
   localStorage.setItem(localStorageKey, JSON.stringify(updatedReminders));
-
-  alert('Atenção, lembrete foi excluído!');
 }
-
-
